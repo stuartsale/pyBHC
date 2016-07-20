503aa22de540fb8295af82ddd2eaff3391f53e9d
Stuart Sale <stuart.sale@gmail.com>
2016-07-20 14:57:04 +0100

    Add a setup.py
    This adds a setup.py file to install pyBHC properly.

---
+++ setup.py
@@ -0,0 +1,70 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README')) as f:
+    long_description = f.read()
+
+
+setup(
+    name='sample',
+
+#    # No version number yet
+#    version='',
+
+    description='Bayesian Hierarchical clustering in python',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='github.com/stuartsale/pyBHC',
+
+    # Author details
+    author='Stuart Sale',
+    author_email='stuart.sale@gmail.com',
+
+    # Choose your license
+    license='BSD',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+#        Not yet tested on Python 3
+#        'Programming Language :: Python :: 3',
+#        'Programming Language :: Python :: 3.3',
+#        'Programming Language :: Python :: 3.4',
+#        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='bayesian clustering',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['numpy', 'scipy'],
+
+)
